5bcb23f10ee8053884f3b09624be86a6f3d8909e
lauren henry <laurendhenry3@gmail.com>
2024-03-12 03:20:19 -0400

    basic login logic

---
+++ app/src/main/java/com/example/cs2340project2/ui/login/LoginAdapter.java
@@ -0,0 +1,39 @@
+package com.example.cs2340project2.ui.login;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.cs2340project2.ui.SignupTabFragment;
+
+public class LoginAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+    int totalTabs;
+    public LoginAdapter(FragmentManager fm, Context context, int totalTabs) {
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position){
+        switch (position){
+            case 0:
+                return new LoginTabFragment();
+            case 1:
+                return new SignupTabFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
--- app/src/main/java/com/example/cs2340project2/ui/login/LoginTabFragment.java
+++ app/src/main/java/com/example/cs2340project2/ui/login/LoginTabFragment.java
@@ -1,4 +1,4 @@
-package com.example.cs2340project2.ui;
+package com.example.cs2340project2.ui.login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.cs2340project2.R;
 
-public class SignupTabFragment extends Fragment {
+public class LoginTabFragment extends Fragment {
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_frag, container, false);
 
