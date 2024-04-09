4f8ef884d48acd42884aa3734afb02e81c274133
braianguzman <braianaliskair@gmail.com>
2024-04-09 16:56:51 -0400

    Fixed some erros

--- app/src/main/java/com/example/cs2340project2/Wrapped1.java
+++ app/src/main/java/com/example/cs2340project2/Wrapped1.java
@@ -1,9 +1,8 @@
 package com.example.cs2340project2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Wrapped1 extends AppCompatActivity {
@@ -12,13 +11,16 @@ public class Wrapped1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wrapped1);
 
-        View myBackgroundView = findViewById(R.id.myBackgroundView);
-        myBackgroundView.setBackgroundResource(getRandomDrawable1());
+        //View myBackgroundView = findViewById(R.id.myBackgroundView);
+        //myBackgroundView.setBackgroundResource(getRandomDrawable1());
     }
 
+    /*
     private int getRandomDrawable1() {
         int[] drawables = new int[] {R.drawable.topsongs1, R.drawable.topsongs2, R.drawable.topsongs3, R.drawable.topsongs4};
         int randomIndex = new Random().nextInt(drawables.length);
         return drawables[randomIndex];
     }
+
+     */
 }
--- app/src/main/java/com/example/cs2340project2/Wrapped2.java
+++ app/src/main/java/com/example/cs2340project2/Wrapped2.java
@@ -1,9 +1,8 @@
 package com.example.cs2340project2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Wrapped2 extends AppCompatActivity {
@@ -12,13 +11,16 @@ public class Wrapped2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wrapped2);
 
-        View myBackgroundView = findViewById(R.id.myBackgroundView);
-        myBackgroundView.setBackgroundResource(getRandomDrawable());
+        //View myBackgroundView = findViewById(R.id.myBackgroundView);
+        //myBackgroundView.setBackgroundResource(getRandomDrawable());
     }
 
+    /*
     private int getRandomDrawable() {
         int[] drawables = new int[] {R.drawable.wrapped1purple, R.drawable.wrapped2orange, R.drawable.wrapped3blue, R.drawable.wrapped4yellow};
         int randomIndex = new Random().nextInt(drawables.length);
         return drawables[randomIndex];
     }
+
+     */
 }
