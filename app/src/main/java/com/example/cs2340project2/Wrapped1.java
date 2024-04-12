82a7ba96e03f3a74e9be1d23836cde44a69f2e8a
Jacob Truong <jacob.truong25@gmail.com>
2024-04-11 22:27:06 -0400

    wrapped activity

--- app/src/main/java/com/example/cs2340project2/Wrapped1.java
+++ app/src/main/java/com/example/cs2340project2/Wrapped1.java
@@ -2,6 +2,7 @@ package com.example.cs2340project2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,12 +13,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.snackbar.Snackbar;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class Wrapped1 extends AppCompatActivity {
 
     private TextView artist1, artist2, artist3, artist4, artist5;
+    private ImageView imageView1, imageView2, imageView3, imageView4, imageView5;
+
     private ActivityResultLauncher<Intent> spotifyAuthResLauncher;
 
     @Override
@@ -32,6 +36,13 @@ public class Wrapped1 extends AppCompatActivity {
         artist3 = findViewById(R.id.artist3);
         artist4 = findViewById(R.id.artist4);
         artist5 = findViewById(R.id.artist5);
+
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+
 
         // Fetch top songs
         spotifyAuthResLauncher = registerForActivityResult(
@@ -50,6 +61,17 @@ public class Wrapped1 extends AppCompatActivity {
                                         artist3.setText(topArtists.get(2).getArtist());
                                         artist4.setText(topArtists.get(3).getArtist());
                                         artist5.setText(topArtists.get(4).getArtist());
+                                        String imageUrlString1 = topArtists.get(0).getImageUrl();
+                                        String imageUrlString2 = topArtists.get(1).getImageUrl();
+                                        String imageUrlString3 = topArtists.get(2).getImageUrl();
+                                        String imageUrlString4 = topArtists.get(3).getImageUrl();
+                                        String imageUrlString5 = topArtists.get(4).getImageUrl();
+                                        Picasso.get().load(imageUrlString1).into(imageView1);
+                                        Picasso.get().load(imageUrlString2).into(imageView2);
+                                        Picasso.get().load(imageUrlString3).into(imageView3);
+                                        Picasso.get().load(imageUrlString4).into(imageView4);
+                                        Picasso.get().load(imageUrlString5).into(imageView5);
+
                                     } else {
                                         Toast.makeText(Wrapped1.this, "Must have listened to at least 5 artists", Toast.LENGTH_SHORT).show();
                                     }
